1b965b779a1197ade2b70ed6f0a116d1caef5df4
taka <>
2020-09-10 16:57:25 +0900

    リポジトリがエラーを返すように修正

--- go/src/MyPIPE/domain/repository/CommentRepository.go
+++ go/src/MyPIPE/domain/repository/CommentRepository.go
@@ -3,8 +3,8 @@ package repository
 import "MyPIPE/domain/model"
 
 type CommentRepository interface {
-	GetAll() []model.Comment
-	FindById() *model.Comment
-	FindByUserId(userId model.UserID) []model.Comment
-	FindByMovieId(movieId model.MovieID) []model.Comment
+	GetAll() ([]model.Comment, error)
+	FindById() (*model.Comment, error)
+	FindByUserId(userId model.UserID) ([]model.Comment, error)
+	FindByMovieId(movieId model.MovieID) ([]model.Comment, error)
 }
--- go/src/MyPIPE/domain/repository/MovieRepository.go
+++ go/src/MyPIPE/domain/repository/MovieRepository.go
@@ -3,7 +3,7 @@ package repository
 import "MyPIPE/domain/model"
 
 type MovieRepository interface {
-	GetAll() []model.Movie
-	FindById(id model.MovieID) *model.Movie
-	FindByUserId(userId model.MovieID) *model.Movie
+	GetAll() ([]model.Movie, error)
+	FindById(id model.MovieID) (*model.Movie, error)
+	FindByUserId(userId model.MovieID) (*model.Movie, error)
 }
