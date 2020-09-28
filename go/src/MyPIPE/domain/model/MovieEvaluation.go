590158c9e3eaadf0b50af770658f05e09653795a
taka <>
2020-09-29 00:11:00 +0900

    動画の評価を表現するMovieEvaluationエンティティを実装

---
+++ go/src/MyPIPE/domain/model/MovieEvaluation.go
@@ -0,0 +1,38 @@
+package model
+
+import(
+	"errors"
+)
+
+type Evaluation uint64
+
+func NewEvaluation(evaluation string)(Evaluation,error){
+	switch evaluation {
+	case "good":
+		return Evaluation(0),nil
+	case "bad":
+		return Evaluation(1),nil
+	case "default":
+		return Evaluation(2),nil
+	}
+	return Evaluation(999),errors.New("Invalid Evaluation.")
+}
+
+type MovieEvaluation struct{
+	UserID UserID
+	MovieID MovieID
+	Evaluation Evaluation
+}
+
+func NewMovieEvaluation(userId UserID,movieId MovieID,evaluation Evaluation)*MovieEvaluation{
+	return &MovieEvaluation{
+		UserID: userId,
+		MovieID: movieId,
+		Evaluation: evaluation,
+	}
+}
+
+func (m *MovieEvaluation)EvaluateMovie(evaluation Evaluation)error{
+	m.Evaluation = evaluation
+	return nil
+}
