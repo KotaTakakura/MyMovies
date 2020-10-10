9003b965db0c610016c70151f7b3ae7ffec56e65
taka <>
2020-10-11 02:14:26 +0900

    コメント取得API用のルーティング

--- go/src/MyPIPE/main.go
+++ go/src/MyPIPE/main.go
@@ -66,6 +66,7 @@ func main() {
 	router.GET("/refresh_token", authMiddleware.RefreshHandler)
 	router.POST("/new", handler.TemporaryRegisterUser)
 	router.POST("/register", handler.RegisterUser)
+	router.GET("/comments",handler.GetComments)
 
 	auth := router.Group("/auth/api/v1")
 	auth.Use(authMiddleware.MiddlewareFunc())
