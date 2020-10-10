4e31eb20d8f6aed9110c71a336675bb19ef40e65
taka <>
2020-10-11 02:15:33 +0900

    コメント取得用APIのハンドラー

---
+++ go/src/MyPIPE/handler/GetComments.go
@@ -0,0 +1,54 @@
+package handler
+
+import (
+	"MyPIPE/domain/model"
+	queryService_infra "MyPIPE/infra/queryService"
+	"MyPIPE/usecase"
+	"encoding/json"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
+)
+
+func GetComments(c *gin.Context){
+	var getCommentsJson GetCommentsJson
+	c.Bind(&getCommentsJson)
+
+	validationErrors := make(map[string]string)
+	movieIdInt,_ := strconv.ParseUint(c.Query("movie_id"), 10, 64)
+	movieId,movieIdErr := model.NewMovieID(movieIdInt)
+	if movieIdErr != nil{
+		validationErrors["movie_id"] = movieIdErr.Error()
+	}
+
+	if len(validationErrors) != 0{
+		validationErrors,_ := json.Marshal(validationErrors)
+		c.JSON(http.StatusBadRequest, gin.H{
+			"result": "Validation Error.",
+			"messages": string(validationErrors),
+		})
+		c.Abort()
+		return
+	}
+
+	getCommentsDTO := usecase.NewGetCommentsDTO(movieId)
+	commentQueryService := queryService_infra.NewCommentQueryService()
+	getCommentsUsecase := usecase.NewGetComments(commentQueryService)
+	comments := getCommentsUsecase.Get(*getCommentsDTO)
+
+	jsonResult, jsonMarshalErr := json.Marshal(comments)
+	if jsonMarshalErr != nil{
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"result": "Validation Error.",
+			"messages": jsonMarshalErr.Error(),
+		})
+		c.Abort()
+		return
+	}
+
+	c.JSON(http.StatusOK, string(jsonResult))
+}
+
+type GetCommentsJson struct{
+	MovieID uint64
+}
