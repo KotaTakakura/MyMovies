9d20cba5f0757189302f8777c7fe917ab5543a2e
taka <>
2020-10-25 03:28:34 +0900

    usecaseのテスト実装

---
+++ go/src/MyPIPE/Test/usecase/CheckUserAlreadyLikedMovie_test.go
@@ -0,0 +1,39 @@
+package test
+
+import (
+	mock_repository "MyPIPE/Test/mock/repository"
+	"MyPIPE/domain/model"
+	"MyPIPE/usecase"
+	"github.com/golang/mock/gomock"
+	"testing"
+)
+
+func TestCheckUserAlreadyLikedMovie(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	movieEvaluationRepository := mock_repository.NewMockMovieEvaluationRepository(ctrl)
+	checkUserAlreadyLikedMovieUsecase := usecase.NewCheckUserAlreadyLikedMovie(movieEvaluationRepository)
+
+	trueCases := []usecase.CheckUserAlreadyLikedMovieFindDTO{
+		usecase.CheckUserAlreadyLikedMovieFindDTO{
+			UserID:  model.UserID(10),
+			MovieID: model.MovieID(100),
+		},
+	}
+
+	for _, trueCase := range trueCases {
+		movieEvaluationRepository.EXPECT().FindByUserIdAndMovieIdAndEvaluation(trueCase.UserID, trueCase.MovieID, model.Evaluation(0)).Return(
+			&model.MovieEvaluation{
+				UserID:     trueCase.UserID,
+				MovieID:    trueCase.MovieID,
+				Evaluation: model.Evaluation(0),
+			},
+		)
+
+		result := checkUserAlreadyLikedMovieUsecase.Find(&trueCase)
+		if !result {
+			t.Fatal("Usecase Error.")
+		}
+	}
+}
