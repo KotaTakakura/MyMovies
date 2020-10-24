812b8ea76e0a5d92951b66134694b3cbb2299294
taka <>
2020-10-25 01:26:26 +0900

    Usecaseテスト実装

--- go/src/MyPIPE/Test/usecase/AddPlayListItem_test.go
+++ go/src/MyPIPE/Test/usecase/AddPlayListItem_test.go
@@ -1,54 +1,63 @@
 package test
 
 import (
+	mock_factory "MyPIPE/Test/mock/factory"
+	mock_repository "MyPIPE/Test/mock/repository"
+	"MyPIPE/domain/model"
+	"MyPIPE/usecase"
+	"github.com/golang/mock/gomock"
 	"testing"
+	"reflect"
 )
 
 func TestAddPlayListItem(t *testing.T) {
-	//ctrl := gomock.NewController(t)
-	//defer ctrl.Finish()
-	//
-	//playListItemAddJson := &usecase.AddPlayListItemAddJson{
-	//	PlayListID: model.PlayListID(100),
-	//	UserID:     model.UserID(50),
-	//	MovieID:    model.MovieID(30),
-	//}
-	//
-	////PlayListRepository Mock
-	//PlayListRepository := mock_repository.NewMockPlayListRepository(ctrl)
-	//PlayListRepository.EXPECT().FindByID(playListItemAddJson.PlayListID).
-	//	Return(&model.PlayList{
-	//	ID:            model.PlayListID(100),
-	//	UserID:        playListItemAddJson.UserID,
-	//	Name:          "TestPlayList",
-	//	PlayListItems: []model.MovieID{},
-	//	CreatedAt:     time.Time{},
-	//	UpdatedAt:     time.Time{},
-	//},nil)
-	//PlayListRepository.EXPECT().Save(gomock.Any()).DoAndReturn(
-	//	func(playList *model.PlayList)error{
-	//		if playList.UserID != playListItemAddJson.UserID{
-	//			t.Error("UserID Invalid.")
-	//		}
-	//
-	//		for _,v := range playList.PlayListItems{
-	//			if v == playListItemAddJson.MovieID{
-	//				return nil
-	//			}
-	//		}
-	//
-	//		t.Error("Item Doesn't Contained.")
-	//		return nil
-	//	})
-	//
-	////PlayListService Mock
-	//PlayListService := mock_domain_service.NewMockIPlayListService(ctrl)
-	//PlayListService.EXPECT().CanAddItem(playListItemAddJson.MovieID).Return(true)
-	//
-	//playListItemAddUsecase := usecase.NewAddPlayListItem(PlayListRepository,PlayListService)
-	//err := playListItemAddUsecase.AddPlayListItem(*playListItemAddJson)
-	//if err != nil{
-	//	t.Error("AddPlayListItemUsecase Error.")
-	//}
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
 
+	playListRepsitory := mock_repository.NewMockPlayListRepository(ctrl)
+	playListMovieRepository := mock_repository.NewMockPlayListMovieRepository(ctrl)
+	playListMovieFactory := mock_factory.NewMockIPlayListMovie(ctrl)
+	addPlayListItemUsecase := usecase.NewAddPlayListItem(playListRepsitory,playListMovieRepository,playListMovieFactory)
+
+	trueCases := []usecase.AddPlayListItemAddJson{
+		usecase.AddPlayListItemAddJson{
+			PlayListID: model.PlayListID(10),
+			UserID:     model.UserID(20),
+			MovieID:    model.MovieID(30),
+		},
+	}
+
+	for _,trueCase := range trueCases{
+		playListRepsitory.EXPECT().FindByID(trueCase.PlayListID).Return(&model.PlayList{
+			ID:            trueCase.PlayListID,
+			UserID:         trueCase.UserID,
+			Name:           "TestName",
+			Description:    "TestDescription",
+			PlayListMovies: nil,
+		},nil)
+
+		playListMovieFactory.EXPECT().CreatePlayListMovie(trueCase.PlayListID,trueCase.MovieID).Return(&model.PlayListMovie{
+			PlayListID: trueCase.PlayListID,
+			MovieID:    trueCase.MovieID,
+			Order:      0,
+		},nil)
+
+		playListMovieRepository.EXPECT().Save(gomock.Any()).DoAndReturn(func(data interface{})error{
+			if reflect.TypeOf(data) != reflect.TypeOf(&(model.PlayListMovie{})) {
+				t.Fatal("Type Not Match.")
+			}
+			if data.(*model.PlayListMovie).PlayListID != trueCase.PlayListID {
+				t.Fatal("PlayListID Not Match.")
+			}
+			if data.(*model.PlayListMovie).MovieID != trueCase.MovieID {
+				t.Fatal("MovieID Not Match.")
+			}
+			return nil
+		})
+
+		result := addPlayListItemUsecase.AddPlayListItem(&trueCase)
+		if result != nil{
+			t.Fatal("Usecase Error.")
+		}
+	}
 }
