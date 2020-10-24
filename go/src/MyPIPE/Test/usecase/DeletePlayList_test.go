04282b4cb92502c628977a0c7269cb9d39caa245
taka <>
2020-10-25 05:54:58 +0900

    usecaseのテスト実装

---
+++ go/src/MyPIPE/Test/usecase/DeletePlayList_test.go
@@ -0,0 +1,33 @@
+package test
+
+import (
+	mock_repository "MyPIPE/Test/mock/repository"
+	"MyPIPE/domain/model"
+	"MyPIPE/usecase"
+	"github.com/golang/mock/gomock"
+	"testing"
+)
+
+func TestDeletePlayList(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	playListRepository := mock_repository.NewMockPlayListRepository(ctrl)
+	deletePlayListUsecase := usecase.NewDeletePlayList(playListRepository)
+
+	trueCases := []usecase.DeletePlayListDTO{
+		usecase.DeletePlayListDTO{
+			UserID:     model.UserID(10),
+			PlaylistID: model.PlayListID(20),
+		},
+	}
+
+	for _, trueCase := range trueCases {
+		playListRepository.EXPECT().Remove(trueCase.UserID, trueCase.PlaylistID).Return(nil)
+
+		result := deletePlayListUsecase.Delete(&trueCase)
+		if result != nil {
+			t.Fatal("Usecase Error.")
+		}
+	}
+}
