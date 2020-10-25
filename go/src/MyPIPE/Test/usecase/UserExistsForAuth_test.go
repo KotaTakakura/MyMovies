ade5a113c19445002e7fcf256e694f4fe093b324
taka <>
2020-10-25 11:13:49 +0900

    usecaseのテスト実装

---
+++ go/src/MyPIPE/Test/usecase/UserExistsForAuth_test.go
@@ -0,0 +1,37 @@
+package test
+
+import (
+	mock_repository "MyPIPE/Test/mock/repository"
+	"MyPIPE/domain/model"
+	"MyPIPE/usecase"
+	"github.com/golang/mock/gomock"
+	"testing"
+)
+
+func TestUserExistsForAuth(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	userRepository := mock_repository.NewMockUserRepository(ctrl)
+	userExistsForAuthUsecase := usecase.NewUserExists(userRepository)
+
+	trueCases := []struct {
+		email    model.UserEmail
+		password string
+	}{
+		{email: model.UserEmail("Test@Email.com"), password: "TestPasword"},
+	}
+
+	for _, trueCase := range trueCases {
+		password, _ := model.NewUserPassword(trueCase.password)
+		userRepository.EXPECT().FindByEmail(trueCase.email).Return(&model.User{
+			Password: password,
+			Email:    trueCase.email,
+		}, nil)
+
+		user, err := userExistsForAuthUsecase.CheckUserExistsForAuth(trueCase.email, trueCase.password)
+		if err != nil || user == nil {
+			t.Fatal("Usecase Error.")
+		}
+	}
+}
