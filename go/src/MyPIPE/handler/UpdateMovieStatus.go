807541c4a01fee536f04881b232c53a05d3af4c0
taka <>
2020-11-03 09:23:55 +0900

    修正

--- go/src/MyPIPE/handler/UpdateMovieStatus.go
+++ go/src/MyPIPE/handler/UpdateMovieStatus.go
@@ -3,7 +3,10 @@ package handler
 import (
 	"MyPIPE/domain/model"
 	"MyPIPE/usecase"
+	"encoding/json"
 	"fmt"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sns"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
@@ -19,9 +22,34 @@ func NewUpdateMovieStatus(updateMovieStatusUsecase usecase.IUpdateMovie) *Update
 }
 
 func (u UpdateMovieStatus) UpdateMovieStatus(c *gin.Context) {
+	requestBodyBuf := make([]byte, 2048)
+	n, _ := c.Request.Body.Read(requestBodyBuf)
+	request := string(requestBodyBuf[0:n])
+
 	var updateMovieStatusJson UpdateMovieStatusJson
-	c.Bind(&updateMovieStatusJson)
+	err := json.Unmarshal([]byte(request), &updateMovieStatusJson)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 	fmt.Println(updateMovieStatusJson)
+
+	if updateMovieStatusJson.Type == "SubscriptionConfirmation"{
+		sess := session.Must(session.NewSession())
+		svc := sns.New(sess)
+		_,err := svc.ConfirmSubscription(&sns.ConfirmSubscriptionInput{
+			Token:                     &updateMovieStatusJson.Token,
+			TopicArn:                  &updateMovieStatusJson.TopicArn,
+		})
+		if err != nil{
+			c.JSON(http.StatusBadRequest, gin.H{
+				"result":   "Failed.",
+				"messages": err.Error(),
+			})
+			c.Abort()
+			return
+		}
+	}
 
 	movieId, movieIdErr := model.NewMovieID(updateMovieStatusJson.MovieID)
 	if movieIdErr != nil {
@@ -52,4 +80,6 @@ func (u UpdateMovieStatus) UpdateMovieStatus(c *gin.Context) {
 type UpdateMovieStatusJson struct {
 	MovieID uint64 `json:"movie_id"`
 	Type string `json:"Type"`
+	TopicArn string `json:"TopicArn"`
+	Token string `json:"Token"`
 }
